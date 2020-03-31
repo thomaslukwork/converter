c630a9d6988184120531d77e00979e88192e58b3
Thomas Luk <thomasluk@gmail.com>
2020-03-31 16:58:59 +0100

    add unit model and ion-select

--- src/app/temperatureTab/temperatureTab.page.ts
+++ src/app/temperatureTab/temperatureTab.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Unit } from '../unit.model';
 
 @Component({
   selector: 'app-temperatorTab',
@@ -7,6 +8,51 @@ import { Component } from '@angular/core';
 })
 export class TemperatureTabPage {
 
-  constructor() {}
+  customPopoverOptions: any = {
+    header: 'Temperature',
+    subHeader: 'Select a unit is going to be converted'
+  };
+
+  fromSelectedId: string;
+  toSelectedId: string;
+
+  // selectedUnit: Unit;
+  units: Unit[] = [
+    {
+      type: 'temp',
+      id: 'ce',
+      unitDesc: 'Celsius',
+      ratio: 1
+    },
+    {
+      type: 'temp',
+      id: 'fa',
+      unitDesc: 'Fahrenheit',
+      ratio: 1
+    }
+  ];
+
+
+  onChange = () => {
+    console.log('from id == ' + this.fromSelectedId);
+    console.log('to id == ' + this.toSelectedId);
+
+    // console.log('selectedUnit id = ' + this.selectedUnit.id);
+  }
+
+
+  // public optionsFn(): void {
+  //   //here item is an object 
+  //   console.log(this.unit);
+
+  // }
+  // public onChange(): void {
+  //   console.log('Selected:' + this.place);
+  //   let item = this.place;
+  //   console.log('Selected:' + item.id);
+
+  // }
+
+  constructor() { }
 
 }
---
+++ src/app/unit.model.ts
@@ -0,0 +1,6 @@
+export interface Unit {
+    type: string;
+    id: string;
+    unitDesc: string;
+    ratio: number;
+}
