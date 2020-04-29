740c2850780508aee1ef4291df1c4516eeda0de2
Thomas Luk <thomasluk@gmail.com>
2020-04-29 16:43:50 +0100

    initial

---
+++ src/app/capacityTab/capacityTab.page.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { CapacityTabPage } from './capacityTab.page';
+
+describe('CapacityTabPage', () => {
+  let component: CapacityTabPage;
+  let fixture: ComponentFixture<CapacityTabPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CapacityTabPage],
+      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CapacityTabPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/capacityTab/capacityTab.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-capacity-tab',
+  templateUrl: './capacityTab.page.html',
+  styleUrls: ['./capacityTab.page.scss'],
+})
+export class CapacityTabPage {
+
+  constructor() { }
+
+}
